bb8245a30751ca6053768ecabfb3103f10b022e3
shawon13 <rakibarg1519@gmail.com>
2024-03-05 12:56:51 +0600

    install and setup react router

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Router/Router.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
